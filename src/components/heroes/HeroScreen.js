edbe5ab0bbc5a3fcef33fe75ac766bf2912dafd2
David Portilla <david_portilla@hotmail.com>
2020-10-02 13:20:54 -0500

    adding detail view for Heroe

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Redirect, useParams } from 'react-router-dom'
 import { getHeroesById } from '../../selectors/getHeroById'
 
-const HeroScreen = () => {
+const HeroScreen = ({ history }) => {
 
   const { heroeId } = useParams()
   const hero = getHeroesById(heroeId)
@@ -11,10 +11,46 @@ const HeroScreen = () => {
     return <Redirect to="/" />
   }
 
+  const {
+    superhero,
+    publisher,
+    alter_ego,
+    first_appearance,
+    characters,
+  } = hero
+
+  const handleReturn = () => {
+    if (history.length <= 2) {
+      history.push('/')
+    } else {
+      history.goBack()
+    }
+  }
+
   return (
-    <>
-      <h1>HeroScreen</h1>
-    </>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img
+          src={ `../assets/heroes/${ heroeId }.jpg` }
+          alt={ superhero }
+          className="img-thumbnail"
+        />
+      </div>
+      <div className="col-8">
+        <h2>{ superhero }</h2>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item"><b>Alter ego: </b>{ alter_ego }</li>
+          <li className="list-group-item"><b>Publisher: </b>{ publisher }</li>
+          <li className="list-group-item"><b>First Appearance: </b>{ first_appearance }</li>
+        </ul>
+        <h3 className="mt-4"> Characters</h3>
+        <p>{ characters }</p>
+        <button
+          className="btn btn-outline-info"
+          onClick={ handleReturn }
+        > Return</button>
+      </div>
+    </div>
   )
 }
 
