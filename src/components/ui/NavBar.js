704fbada96998d1359360dd469bf5623938606bc
David Portilla <david_portilla@hotmail.com>
2020-10-02 16:16:00 -0500

    update app name

--- src/components/ui/NavBar.js
+++ src/components/ui/NavBar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
         className="navbar-brand"
         to="/"
       >
-        Asociaciones
+        Heroes App
             </Link>
 
       <div className="navbar-collapse">
