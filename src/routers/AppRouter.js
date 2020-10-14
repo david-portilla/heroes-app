4205b84cfd9ebf894a8f8a35c4e09fc03ac1bd74
David Portilla <david_portilla@hotmail.com>
2020-10-14 18:16:04 -0500

    adding protected URLs

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { useContext } from 'react';
+import { AuthContext } from '../auth/AuthContext';
 import {
   BrowserRouter as Router,
   Switch,
@@ -7,14 +9,22 @@ import {
 
 import LoginScreen from '../components/login/LoginScreen';
 import DashboardRoutes from './DashboardRoutes';
+import PrivateRoute from './PrivateRoute';
 
 export const AppRouter = () => {
+
+  const { user } = useContext(AuthContext)
+
   return (
     <Router>
       <div>
         <Switch>
           <Route exact path="/login" component={ LoginScreen } />
-          <Route path="/" component={ DashboardRoutes } />
+          <PrivateRoute
+            path="/"
+            component={ DashboardRoutes }
+            isAuthenticated={ user.logged }
+          />
         </Switch>
       </div>
     </Router>
