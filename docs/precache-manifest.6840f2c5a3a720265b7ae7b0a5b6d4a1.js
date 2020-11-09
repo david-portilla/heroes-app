af8e865cc7138691f977694f762a2812a31291ea
David Portilla <david_portilla@hotmail.com>
2020-11-09 14:16:43 -0500

    preparing files for deploy

---
+++ docs/precache-manifest.6840f2c5a3a720265b7ae7b0a5b6d4a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca52ec85e52202424b57f234def6ca8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d34909969c24697bcfcf",
+    "url": "/static/js/2.ffeae846.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.ffeae846.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5309fcba5230c7307dad",
+    "url": "/static/js/main.6c402c33.chunk.js"
+  },
+  {
+    "revision": "94d7ca25d4df19621824",
+    "url": "/static/js/runtime-main.b7f813af.js"
+  }
+]);
